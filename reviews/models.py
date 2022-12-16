07bb38dfebaee5c02d426aef42701d02499f9dc5
Kalys <jsvsjsvshsksb@gmail.com>
2022-12-16 12:15:17 +0600

    review_models

---
+++ reviews/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from main.models import Post, User
+
+
+class PostComment(models.Model):
+    user_id = models.ForeignKey(User, related_name='comments',on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, related_name='comments',on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
